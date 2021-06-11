4a242e1799488b7b4cdaf28ebb43ce1f68511ba0
annasajkh <annaskherunas06@gmail.com>
2021-06-11 19:19:57 +0700

    fixing range issue

--- cmds.py
+++ cmds.py
@@ -26,6 +26,11 @@ class Command:
     def crop(self,value):
         value = edit_functions.to_array(value, 4)
 
+        value[0] = edit_functions.clamp(int(value),0,100)
+        value[1] = edit_functions.clamp(int(value),0,100)
+        value[2] = edit_functions.clamp(int(value),0,100)
+        value[3] = edit_functions.clamp(int(value),0,100)
+
         self.img = self.img.crop(  (value[0] / 100 * self.img.size[0], 
                                     value[1] / 100 * self.img.size[1], 
                                     value[2] / 100 * self.img.size[0], 
